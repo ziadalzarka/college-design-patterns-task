c12918a14cc03c22a7397b16b7b33115de3f3394
Ziad Alzarka <ziadalzarka@gmail.com>
2020-12-19 12:49:20 +0200

    feat: patterns

--- Program.cs
+++ Program.cs
@@ -2,11 +2,37 @@
 
 namespace data_structures
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("1- Proxy Design Pattern");
+      Console.WriteLine("------------------------");
+      ProxyPattern.Program.Main();
+
+      Console.WriteLine("");
+      Console.WriteLine("**************************************");
+      Console.WriteLine("");
+
+      Console.WriteLine("2- Facade Design Pattern");
+      Console.WriteLine("------------------------");
+      FacadePattern.Program.Main();
+
+      Console.WriteLine("");
+      Console.WriteLine("**************************************");
+      Console.WriteLine("");
+
+      Console.WriteLine("3- Observer Design Pattern");
+      Console.WriteLine("------------------------");
+      ObserverPattern.Program.Main();
+
+      Console.WriteLine("");
+      Console.WriteLine("**************************************");
+      Console.WriteLine("");
+
+      Console.WriteLine("4- Adapter Design Pattern");
+      Console.WriteLine("------------------------");
+      AdapterPattern.Program.Main();
     }
+  }
 }
